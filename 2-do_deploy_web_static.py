3b70dbcf2008edbabeb8608adc43708add366abd
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-26 15:32:10 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,8 +2,11 @@
 """
 Write a Fabric script that generates a .tgz archive from the contents
 """
-from fabric.operations import local
+from fabric.operations import local, run, put
 from datetime import datetime
+import os
+from fabric.api import env
+import re
 
 
 def do_pack():
@@ -22,18 +25,21 @@ def do_deploy(archive_path):
     """ Script that does a lot of magic =) like penn and teller """
     if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
         return False
-    put(archive_path, '/tmp/')
-    m = archive_path.replace('.tgz', '')
-    m = m.replace('versions/', '')
-    run('mkdir -p /data/web_static/releases/{}/'.format(m))
-    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-        .format(m, m))
-    run('rm /tmp/{}.tgz'.format(m))
-    run('mv /data/web_static/releases/{}/web_static/* '.format(m) +
-        '/data/web_static/releases/{}/'.format(m))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(m))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-        .format(m))
-    print('New version successfuly deployed')
+    try:
+        put(archive_path, "/tmp")
+        fileonly = os.path.basename(archive_path)
+        filename = os.path.splitext(fileonly)[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        from_here = "/tmp/{}".format(fileonly)
+        to_here = "/data/web_static/releases/{}/".format(filename)
+        run("tar -xzf {} -C {}".format(from_here, to_here))
+        run('rm /tmp/{}'.format(fileonly))
+        run('mv {}web_static/* {}'.format(to_here, to_here))
+        run('rm -rf {}web_static'.format(to_here))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(to_here))
+        print('New version deployed!')
+        return True
+    except:
+        return False
     return True
