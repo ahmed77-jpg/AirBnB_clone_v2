2615d3a2a12931d0f0a07b00829e58144914089e
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-26 14:58:06 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,14 +5,15 @@ Write a Fabric script that generates a .tgz archive from the contents
 from fabric.operations import local
 from datetime import datetime
 
+
 def do_pack():
     """
     function creates a .tgz
     """
     local("mkdir -p versions")
     rtat = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                   capture=True)
+                 .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                 capture=True)
     if rtat.failed:
         return None
     return rtat
