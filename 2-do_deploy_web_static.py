22e1d37b1b4ca27321263e07d3d0b034c8858efe
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-26 15:38:11 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Write a Fabric script that generates a .tgz archive from the contents
 from fabric.operations import local, run, put
 from datetime import datetime
 import os
-from fabric.api import env
+from fabric.api import *
 import re
 
 
@@ -23,23 +23,29 @@ def do_pack():
 
 def do_deploy(archive_path):
     """ Script that does a lot of magic =) like penn and teller """
-    if not os.path.exists(archive_path) and not os.path.isfile(archive_path):
+    if (archive_path is False or archive_path is None):
         return False
     try:
-        put(archive_path, "/tmp")
-        fileonly = os.path.basename(archive_path)
-        filename = os.path.splitext(fileonly)[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        from_here = "/tmp/{}".format(fileonly)
-        to_here = "/data/web_static/releases/{}/".format(filename)
-        run("tar -xzf {} -C {}".format(from_here, to_here))
-        run('rm /tmp/{}'.format(fileonly))
-        run('mv {}web_static/* {}'.format(to_here, to_here))
-        run('rm -rf {}web_static'.format(to_here))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(to_here))
-        print('New version deployed!')
-        return True
+        # this will be the web_static_NUMBERSSSS.tgz
+        last = archive_path.split("/")[-1]
+        # and this will be the stuff without the dot extension
+        foldName = "/data/web_static/releases/" + last.split(".")[0]
+        # overwrites pre-existing remote files without request confirmation
+        put(archive_path, "/tmp/")
+        # make the directory on the server
+        run("sudo mkdir -p {}".format(foldName))
+        # unzips the archive to the folder on the webserver
+        run("sudo tar -xzf /tmp/{} -C {}".format(last, foldName))
+        # deletes archive from web server
+        run("sudo rm /tmp/{}".format(last))
+        # moves the archive out of web static to be removed
+        run("sudo mv {}/web_static/* {}/".format(foldName, foldName))
+        # removes the archive
+        run("sudo rm -rf {}/web_static".format(foldName))
+        # deletes the symbolic link to the web server
+        run("sudo rm -rf /data/web_static/current")
+        # create a new sym link that links to new version of code
+        run("sudo ln -s {} /data/web_static/current".format(foldName))
     except:
         return False
     return True
