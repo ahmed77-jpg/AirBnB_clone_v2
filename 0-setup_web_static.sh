a05dcc803d5f8639a053bbdc6c7cd87d790da762
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-26 14:12:30 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #Write a Bash script that sets up your web servers for the deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-sudo echo "Hey AIrbnb" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo apt-get update -y
+sudo apt-get updgrade -y
+sudo apt-get install -y nginx
+mkdir -p  /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir /data/web_static/shared/
+echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown ubuntu:ubuntu -hR /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
