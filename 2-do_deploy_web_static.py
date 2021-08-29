655891ff1cb212d359062b3e02f443e7a9a1e214
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-29 14:07:20 +0000

    all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,30 +2,50 @@
 """
 Write a Fabric script that generates a .tgz archive from the contents
 """
-from fabric.operations import local, run, put
+from fabric.api import run, put, local, env
 from datetime import datetime
-import os
-from fabric.api import env
-import re
+import os.path
 
 env.hosts = ['34.139.123.27', '34.73.206.129']
 
-def do_deploy(archive_path):
-    """deploy tar package to remote server"""
+def do_pack():
+    """
+     generates a .tgz archive from the contents of the web_static folder of
+     your AirBnB Clone repo, using the function do_pack.
+    """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = 'versions/web_static_' + date + '.tgz'
+    if not (os.path.exists("versions")):
+        local('mkdir -p versions')
+    local('tar -cvzf ' + path + ' web_static')
+    if (os.path.exists(path)):
+        return path
+    return None
 
-    if os.path.exists(archive_path) is False:
-        return False
-    arch_name = archive_path.split('/')[1]
-    arch_name_nex = arch_name.split(".")[0]
-    re_path = "/data/web_static/releases/" + arch_name_nex
-    up_path = '/tmp/' + arch_name
-    put(archive_path, up_path)
-    run('mkdir -p ' + re_path)
-    run('tar -xzf /tmp/{} -C {}/'.format(arch_name, re_path))
-    run('rm {}'.format(up_path))
-    mv = 'mv ' + re_path + '/web_static/* ' + re_path + '/'
-    run(mv)
-    run('rm -rf ' + re_path + '/web_static')
+
+def do_deploy(archive_path):
+    """
+    - Upload the archive to the /tmp/ directory of the web server
+    - iUncompress the archive to the folder /data/web_static/releases/<archive
+    filename without extension> on the web server
+    - Delete the archive from the web server
+    - Delete the symbolic link /data/web_static/current from the web server
+    - Create a new the symbolic link /data/web_static/current on the web
+    server, linked to the new version of your code
+    (/data/web_static/releases/<archive filename without extension>)
+    """
+    if not (os.path.exists(archive_path)):
+            return False
+    archive_name = archive_path.split('/')[1]
+    archive_name_without_ext = archive_path.split('/')[1].split('.')[0]
+    release_path = '/data/web_static/releases/' + archive_name_without_ext
+    upload_path = '/tmp/' + archive_name
+    put(archive_path, upload_path)
+    run('mkdir -p ' + release_path)
+    run('tar -xzf ' + upload_path + ' -C ' + release_path)
+    run('rm ' + upload_path)
+    run('mv ' + release_path + '/web_static/* ' + release_path + '/')
+    run('rm -rf ' + release_path + '/web_static')
     run('rm -rf /data/web_static/current')
-    run('ln -s ' + re_path + ' /data/web_static/current')
+    run('ln -s ' + release_path + ' /data/web_static/current')
     return True
