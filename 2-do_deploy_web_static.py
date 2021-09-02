4f98e40aed3bc8570f28d961ee48853992155ead
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-09-02 22:21:21 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,8 +10,28 @@ import os.path
 env.hosts = ['34.73.206.129', '34.139.123.27']
 
 
+def do_pack():
+    """ pack files into .tgz archive """
+    local("mkdir -p versions")
+    rtat = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                 .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                 capture=True)
+    if rtat.failed:
+        return None
+    return rtat
+
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """
+    - Upload the archive to the /tmp/ directory of the web server
+    - Uncompress the archive to the folder /data/web_static/releases/<archive
+    filename without extension> on the web server
+    - Delete the archive from the web server
+    - Delete the symbolic link /data/web_static/current from the web server
+    - Create a new the symbolic link /data/web_static/current on the web
+    server, linked to the new version of your code
+    (/data/web_static/releases/<archive filename without extension>)
+    """
     if not (os.path.exists(archive_path)):
         return False
     archive_name = archive_path.split('/')[1]
@@ -19,11 +39,11 @@ def do_deploy(archive_path):
     release_path = '/data/web_static/releases/' + archive_name_without_ext
     upload_path = '/tmp/' + archive_name
     put(archive_path, upload_path)
-    run('mkdir -p ' + release_path)
-    run('tar -xzf ' + upload_path + ' -C ' + release_path)
-    run('rm ' + upload_path)
-    run('mv ' + release_path + '/web_static/* ' + release_path + '/')
-    run('rm -rf ' + release_path + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + release_path + ' /data/web_static/current')
+    run('sudo mkdir -p ' + release_path)
+    run('sudo tar -xzf ' + upload_path + ' -C ' + release_path)
+    run('sudo rm ' + upload_path)
+    run('sudo mv ' + release_path + '/web_static/* ' + release_path + '/')
+    run('sudo rm -rf ' + release_path + '/web_static')
+    run('sudo rm -rf /data/web_static/current')
+    run('sudo ln -s ' + release_path + ' /data/web_static/current')
     return True
