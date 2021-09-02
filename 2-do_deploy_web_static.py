b414b5ab8649eeef5993142d068d93d88a9b49ff
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-09-02 12:36:03 +0000

    all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Write a Fabric script that generates a .tgz archive from the contents
 """
 from fabric.api import run, put, local, env
 from datetime import datetime
 import os.path
-
 env.hosts = ['34.139.123.27', '34.73.206.129']
-
 def do_pack():
     """
     function creates a .tgz
@@ -31,18 +29,20 @@ def do_deploy(archive_path):
     server, linked to the new version of your code
     (/data/web_static/releases/<archive filename without extension>)
     """
+    
     if not (os.path.exists(archive_path)):
-            return False
+        return False
     archive_name = archive_path.split('/')[1]
     archive_name_without_ext = archive_path.split('/')[1].split('.')[0]
     release_path = '/data/web_static/releases/' + archive_name_without_ext
     upload_path = '/tmp/' + archive_name
-    put(archive_path, upload_path)
-    run('mkdir -p ' + release_path)
-    run('tar -xzf ' + upload_path + ' -C ' + release_path)
-    run('rm ' + upload_path)
-    run('mv ' + release_path + '/web_static/* ' + release_path + '/')
-    run('rm -rf ' + release_path + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + release_path + ' /data/web_static/current')
+
+    put(archive_path, '/tmp')
+    run('sudo mkdir -p ' + release_path)
+    run('sudo tar -xzf ' + upload_path + ' -C ' + release_path)
+    run('sudo rm ' + upload_path)
+    run('sudo mv ' + release_path + '/web_static/* ' + release_path + '/')
+    run('sudo rm -rf ' + release_path + '/web_static')
+    run('sudo rm -rf /data/web_static/current')
+    run('sudo ln -s ' + release_path + ' /data/web_static/current')
     return True
