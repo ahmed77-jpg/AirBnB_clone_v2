03445c5a48361353061fb6848fb286546becb159
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-09-02 14:09:35 +0000

    all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,30 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Write a Fabric script that generates a .tgz archive from the contents
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
-from fabric.api import run, put, local, env
-from datetime import datetime
-import os.path
-env.hosts = ['34.139.123.27', '34.73.206.129']
-def do_pack():
-    """
-    function creates a .tgz
-    """
-    local("mkdir -p versions")
-    rtat = local("tar -cvzf versions/web_static_{}.tgz web_static"
-                 .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
-                 capture=True)
-    if rtat.failed:
-        return None
-    return rtat
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['34.73.206.129', '34.139.123.27']
+
 
 def do_deploy(archive_path):
-    """
-    - Upload the archive to the /tmp/ directory of the web server
-    - iUncompress the archive to the folder /data/web_static/releases/<archive
-    filename without extension> on the web server
-    - Delete the archive from the web server
-    - Delete the symbolic link /data/web_static/current from the web server
-    - Create a new the symbolic link /data/web_static/current on the web
-    server, linked to the new version of your code
-    (/data/web_static/releases/<archive filename without extension>)
-    """
-    if not (os.path.exists(archive_path)):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    archive_name = archive_path.split('/')[1]
-    archive_name_without_ext = archive_path.split('/')[1].split('.')[0]
-    release_path = '/data/web_static/releases/' + archive_name_without_ext
-    upload_path = '/tmp/' + archive_name
-    put(archive_path, upload_path)
-    run('mkdir -p ' + release_path)
-    run('tar -xzf ' + upload_path + ' -C ' + release_path)
-    run('rm ' + upload_path)
-    run('mv ' + release_path + '/web_static/* ' + release_path + '/')
-    run('rm -rf ' + release_path + '/web_static')
-    run('rm -rf /data/web_static/current')
-    run('ln -s ' + release_path + ' /data/web_static/current')
-    return True
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
